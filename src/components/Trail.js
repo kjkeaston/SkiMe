c9d7cfc898dd4a4854a6b33beeba6eb22c847b93
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2018-02-22 13:07:04 -0800

    render posts for a trail, timestamp working, stars working

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Timestamp from 'react-timestamp';
 
 class Trail extends Component {
   constructor() {
@@ -8,6 +9,7 @@ class Trail extends Component {
       currentTrailName: "",
       allPosts: [],
     }
+    this.testFunction = this.testFunction.bind(this);
   }
 
 // https://skime-api.herokuapp.com/trails/${trailID}.json
@@ -22,26 +24,66 @@ class Trail extends Component {
         allPosts: this.state.allPosts.concat(oneTrail.posts)
       })
     });
-    //   this.setState({
-    //     currentResortName: oneResort.name,
-    //     allTrails: this.state.allTrails.concat(oneResort.trails)
-    //   })
-    //   console.log(this.state.allTrails)
-    // })
+  }
+
+  testFunction() {
+    this.state.allPosts.map(eachPost => {
+      });
   }
 
   render() {
+
     return (
       <div className="container">
+      <button onClick={this.testFunction} className="btn btn-outline-warning">Tester Button</button>
         <h1>{this.state.currentTrailName}</h1>
+        
         {this.state.allPosts.map((eachPost) => {
-          return(
+          return (
+            <div key={eachPost.id} className="col-xs-12">
+              <div class="list-group">
+
+        
+          <Link to={ `/posts/${eachPost.id}`} class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1"><strong>Snow Condition:</strong> {eachPost.snow_condition}</h5>
+              <small class="text-muted"><Timestamp time={eachPost.created_at} utc={true} autoUpda te={60} /></small>
+            </div>
+            <p class="mb-1">Overall rating:
+              { Array.from({length: parseInt(eachPost.star_rating)}, (_,i) => i).map(num => {
+                    return (
+                      <i class="far fa-star"></i>
+                    )
+                  })
+              }
+            </p>
+            <small class="text-muted"><strong>Crowd level:</strong> {eachPost.crowd_level}</small>
+          </Link>
+        
+              </div>
+            </div>
+        )
+        })
+      }
+
+        {this.state.allPosts.map((eachPost) => {
+          return (
             <div key={eachPost.id} className="col-xs-12">
               <Link to={ `/posts/${eachPost.id}`} className="btn btn-outline-info">{eachPost.snow_condition}</Link>
+
+              <p>Overall rating:
+                { 
+                  Array.from({length: parseInt(eachPost.star_rating)}, (_,i) => i).map(num => {
+                    return (
+                      <i class="far fa-star"></i>
+                    )
+                  })
+              } </p>
             </div>
             )
           })
         }
+
       </div>
     )
   }
