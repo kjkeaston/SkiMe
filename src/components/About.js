d66b20f718539c2f902f83d8b495ad019c26d455
kjkeaston <kjkeaston@gmail.com>
2018-11-09 15:58:19 -0800

    typo correction

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ class About extends Component {
           Skiing is awesome! Skiing on ice? Not so much! SkiMe sets out to solve the problem of not knowing individual trail conditions until it’s too late.
           <br/>
           <br/>
-          The issue with existing trail reporting is a lack or real-time information. The only way to know the conditions on any given trail at any given time is to actually ski down the run. What this means is that if a trail is really iced up or has poor snow coverage, you won’t know until you are already on your way down the mountain, and at that point, it is too late to turn back.
+          The issue with existing trail reporting is a lack of real-time information. The only way to know the conditions on any given trail at any given time is to actually ski down the run. What this means is that if a trail is really iced up or has poor snow coverage, you won’t know until you are already on your way down the mountain, and at that point, it is too late to turn back.
           <br/>
           <br/>
           For anyone who spends time in the mountains, you know how fast conditions can change and this app aims to solve that problem by providing an interface for users to give real-time updates of trail conditions. The app is tailored for mobile browsers with a simple UI and big buttons to hopefully prevent click errors when navigating the site with gloves.
